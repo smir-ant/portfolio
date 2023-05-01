b4ea659171cc95cf245c095098edf4a0a0b54585
smir-ant <abcsmir.ant@gmail.com>
2023-05-01 23:54:37 +0700

    FLASK, SMTPLIB

---
+++ Сайт 'Обратная связь'/README.md
@@ -0,0 +1,45 @@
+# Сайт "Обратная связь"
+
+###### Проект реализован 03.03.2022
+
+<picture><img src="https://img.shields.io/badge/Flask-025E8C?style=for-the-badge&logo=python&logoColor=white"></picture>
+<picture><img src="https://img.shields.io/badge/smtplib-025E8C?style=for-the-badge&logo=python&logoColor=white"></picture>
+<picture><img src="https://img.shields.io/badge/bootstrap-797979?style=for-the-badge&logo=addthis&logoColor=white"></picture>
+<picture><img src="https://img.shields.io/badge/Jinja2-797979?style=for-the-badge&logo=addthis&logoColor=white"></picture>
+
+<!-- Toggle -->
+<details>
+<summary>
+Внешний вид
+</summary>
+<picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://user-images.githubusercontent.com/84059957/215088292-cf50a16b-422b-43cc-a211-c4169553ca62.png">
+    <source media="(prefers-color-scheme: light)" srcset="https://user-images.githubusercontent.com/84059957/210322548-b635bad5-c53d-4209-a73e-fb0adcc437bf.png">
+    <img height="0.8">
+</picture>
+
+Получаемое сообщение:
+
+<picture><img width="500" src="https://user-images.githubusercontent.com/84059957/235454560-cbe37cee-94d3-444e-85f9-dc167a5eb4bd.png"></picture>
+
+<!-- ● ● ● ● ● -->
+<p align="center"> <picture> <img height="10px" src="https://user-images.githubusercontent.com/84059957/202842039-1764207e-5c8e-486c-8777-76a16ed12048.png"></picture> </p>
+
+Сайт с формой:
+
+<picture><img src="https://user-images.githubusercontent.com/84059957/235479604-0d950f01-b878-4c69-84d0-496611256de1.png"></picture>
+
+<!-- Окончание -->
+<picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://user-images.githubusercontent.com/84059957/215088776-b06bbe95-42fd-4d78-bcae-70cdbeebbbd3.png">
+    <source media="(prefers-color-scheme: light)" srcset="https://user-images.githubusercontent.com/84059957/210319906-4f1e79cb-1a45-4e5c-93e9-ae21e197e0b9.png">
+    <img>
+</picture>
+</details>
+
+<!-- ------------------------ -->
+<picture> <img width="100%" src="https://user-images.githubusercontent.com/84059957/202753342-fd2ddfa9-2939-43a2-976a-b19a9f32905f.png"> </picture>
+
+Идеи для улучшения:
+
+- [ ] Добавить валидацию на почту и телефон
--- Сайт 'Обратная связь'/main.py
+++ Сайт 'Обратная связь'/main.py
@@ -1,33 +1,25 @@
+"""Дисклеймер: если на google, например, вы долго не пользовались аккаунтом, 
+то 'Вход в аккаунт Google через менее защищенные приложения' блокируется :)"""
+
 from flask import Flask, render_template, request
 import requests
-
 import smtplib  # библиотека для работы с почтовым протоколом
 from email.mime.text import MIMEText  # для того чтобы кодировка и кириллица нормальными были
 from email.header import Header  # для темы в письме, тоже на русском
-smtp_host = 'smtp.gmail.com'  # smtp host
-login = "ivanovich.py@gmail.com"  # адрес отрпавителя
-password = "Secret"  # пароль отправителя
-recipients_email = "email@email.here"  # адрес получаетеля
+from dotenv import load_dotenv
+import os
+
+file_env_path = os.path.join(os.path.dirname(__file__), 'secrets.env')  # в этой же папке
+load_dotenv(file_env_path)  # подгружаем env
+smtp_host = os.getenv('SMTP_HOST')  # smtp host
+login = os.getenv('SMTP_LOGIN')  # адрес отрпавителя
+password = os.getenv('SMTP_PASSWORD')  # пароль отправителя
 
 app = Flask(__name__)
-npoint_response = requests.get('https://api.npoint.io/979434c3f92804827510').json()
-print(npoint_response)
-
-@app.route("/")
-def home():
-    return render_template('index.html', all_posts=npoint_response)
-
-
-@app.route("/about")
-def info_about():
-    return render_template('about.html')
-
-
-@app.route("/contact", methods=["POST", "GET"])
+@app.route("/", methods=["POST", "GET"])
 def info_contact():
     if request.method == 'POST':
         data = request.form
-        print(data)
         # обращаемся к атрибуту name="..." внутри input
         print("Имя =", data["name"])
         print("Почта =", data["email"])
@@ -35,27 +27,27 @@ def info_contact():
         print("Сообщение =", data["message"])
 
 
-        msg = MIMEText(f'Имя: {data["name"]}\nПочта: {data["email"]}\nТелефон: {data["phone"]}\nСообщение: {data["message"]}', 'plain', 'utf-8')  # в тексте наше сообщение и ––– ?? plain ??
+        msg = MIMEText(f"""Ваша обратная связь принята. Вот ваши веденные данные:
+                           - Имя: {data["name"]}
+                           - Почта: {data["email"]}
+                           - Телефон: {data["phone"]}
+                           - Сообщение: {data["message"]}""", 'plain', 'utf-8')  # в тексте наше сообщение и ––– ?? plain ??
+        print("MSG:", msg)
         msg['Subject'] = Header('Обратная связь.', 'utf-8')  # тема письма
         msg['From'] = login  # отправитель
-        msg['To'] = recipients_email  # получатель
+        msg['To'] = data["email"]  # (кому отправлю обратную связь)
 
         with smtplib.SMTP(smtp_host, 587, timeout=10) as connection:  # и без '587, timeout=10' сработает
             # connection.set_debuglevel(1)  # устанавливаем подобный дебаг происходящего
             connection.starttls()  # tls - transport layer security - типа обеспечивает безопасность наших сообщений
             connection.login(login, password)  # входим в почту
             connection.sendmail(from_addr=['From'],
-                                to_addrs=recipients_email,
+                                to_addrs=data["email"],
                                 msg=msg.as_string())  # отправляем письмо
 
         return render_template('contact.html', msg_sent=True)
     else:
         return render_template('contact.html', msg_sent=False)
-
-
-@app.route("/post/<int:num>")
-def get_post(num):
-    return render_template('post.html', selected_post=npoint_response[num - 1])
 
 
 if __name__ == '__main__':
