d6ba68a950742e006463c4257db82ff64e811fd2
smir-ant <smir.ant54@gmail.com>
2023-05-09 01:21:12 +0700

    *В разработке*

--- Сайт 'Red7'/static/scripts.js
+++ Сайт 'Red7'/static/scripts.js
@@ -1,20 +1,42 @@
-const header_short = document.querySelector(".header_short");  // укороченная шапка
-const header_full = document.querySelector(".header_full");  // полная шапка
-const header_logos = document.querySelectorAll(".header_logo");
+const headerShort = document.querySelector(".header_short");  // укороченная шапка
+const headerFull = document.querySelector(".header_full");  // полная шапка
+const headerLogos = document.querySelectorAll(".header_logo");
 
-header_logos.forEach(logo => {
+cards_rules = {
+    "red": "старшая",
+    "orange": "одно число",
+    "yellow": "один цвет",
+    "green": "четные",
+    "lightblue": "разные цвета",
+    "blue": "по порядку",
+    "purple": "меньше 4"
+}
+
+// ====================== Смена шапки при нажатии ======================
+headerLogos.forEach(logo => {
     logo.addEventListener('click', () => {
-        header_full.classList.toggle('hidden');
-        header_short.classList.toggle('hidden');
+        headerFull.classList.toggle('hidden');
+        headerShort.classList.toggle('hidden');
     });
 });
-// header_logo.addEventListener('click', () => {
-    
-// })
 
-// header_logo.addEventListener('click', () => {
-//     header_full.classList.toggle('hidden');
-//     header_short.classList.toggle('hidden');
-// })
+
+// ====================== ЗАПОЛНЕНИЕ КАРТ ======================
+let cards = document.querySelectorAll(".card");  // каждая карта игрока (в руке)
+cards.forEach(card => {
+    console.log(card.className)
+    // Пилим класс: "card card-num-3 card-color-lightblue"
+    const cardColor = card.className.split(' ')[2].split('-')[2]; // например: red 
+    const cardValue = card.className.split(' ')[1].split('-')[2]  // например: 2
+
+    // ===== Подстановка правил =====
+    card.querySelector(".rule.left").innerHTML = cards_rules[cardColor]; 
+    card.querySelector(".rule.right").innerHTML = cards_rules[cardColor];
+    
+    // ===== Подстановка числа =====
+    card.querySelector(".main-num").innerHTML = cardValue;
+    card.querySelector(".up-num").innerHTML = cardValue;
+    card.querySelector(".down-num").innerHTML = cardValue;
+})
 
 
